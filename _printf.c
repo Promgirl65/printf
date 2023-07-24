ec1b57ad191e77c9a9b4ecaf64e026174e0da731
MsomiSK <133114393+MsomiSK@users.noreply.github.com>
2023-07-24 22:12:34 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,61 +1,87 @@
-#include <stdio.h>
-#include <stdarg.h>
+#include "main.h"
 
-/*
- * main - _printf.c
- * Description: A simplified implementation of the printf function in C.
- * Author: [Siyabonga Msomi & Balikis Gbemi Olayinka]
+void print_buffer(char buffer[], int *buff_ind);
+
+/**
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
 
 int _printf(const char *format, ...)
+
 {
-    va_list args;
-    va_start(args, format);
 
-    int i = 0;
-    const char *ptr = format;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
+    
+	if (format == NULL)
 
-    while (*ptr != '\0')
-    {
-        if (*ptr == '%')
-        {
-            ptr++;
-            if (*ptr == 'c')
-            {
-                int c = va_arg(args, int);
-                putchar(c);
-                i++;
-            }
-            else if (*ptr == 's')
-            {
-                char *s = va_arg(args, char *);
-                while (*s != '\0')
-                {
-                    putchar(*s);
-                    s++;
-                    i++;
-                }
-            }
-            else if (*ptr == '%')
-            {
-                putchar('%');
-                i++;
-            }
-        }
-        else
-        {
-            putchar(*ptr);
-            i++;
-        }
-        ptr++;
-    }
+		return (-1);
 
-    va_end(args);
-    return count;
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
+
+	{
+
+		if (format[i] != '%')
+
+		{
+
+			buffer[buff_ind++] = format[i];
+
+			if (buff_ind == BUFF_SIZE)
+
+				print_buffer(buffer, &buff_ind);
+
+			/* write(1, &format[i], 1);*/
+
+			printed_chars++;
+
+		}
+		else
+		{
+
+			print_buffer(buffer, &buff_ind);
+	    	flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+
+			printed = handle_print(format, &i, list, buffer,
+
+				flags, width, precision, size);
+
+			if (printed == -1)
+
+				return (-1);
+
+			printed_chars += printed;
+		}
+	}
+
+	print_buffer(buffer, &buff_ind);
+	va_end(list);
+	return (printed_chars);
 }
 
-int main()
+/**
+ * print_buffer - print contents of buffer if it exist..
+ * @buffer: array of chars..
+ * @buff_ind: index which add next char, represents the length..
+ */
+
+void print_buffer(char buffer[], int *buff_ind)
+
 {
-    _printf("Hello, %s! The answer is %d%c\n", "world", 42, '%');
-    return 0;
+
+	if (*buff_ind > 0)
+
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
