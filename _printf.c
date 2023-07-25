be360a6e4bf72cdb41e990363bdc8c37f31dbfd0
Promgirl65 <olayinkabusolami@gmail.com>
2023-07-25 05:25:33 -0700

    printf_project_c16

--- _printf.c
+++ _printf.c
@@ -1,47 +1,53 @@
-#include "main.h"
-
-/**
- * _printf : function that produces output according to a format
- * @format: format specifier
- * Return: the length of the string.
- */
+#include <stdio.h>
+#include <stdarg.h>
 
 int _printf(const char *format, ...)
 {
-	convert_match m[] = {
-		{"%s", printf_string}, {"%c", printf_char},
-		{"%%", printf_37},
-		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
-		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
-		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
-		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
-	};
-
+	int all_set = 0;
+	char c;
+	char *str;
+	
 	va_list args;
-	int i = 0, j, len = 0;
-
 	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
-
-Here:
-	while (format[i] != '\0')
+	
+	while (*format)
 	{
-		j = 13;
-		while (j >= 0)
+		if (*format == '%')
 		{
-			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+			format++;
+			switch (*format)
 			{
-				len += m[j].f(args);
-				i = i + 2;
-				goto Here;
+				case 'c':
+					c = (char) va_arg(args, int);
+					putchar(c);
+					all_set++;
+					break;
+
+				case 's':
+					str = va_arg(args, char*);
+					while (*str)
+					{
+						putchar(*str);
+						str++;
+						all_set++;
+					}
+					break;
+
+				case '%':
+					putchar('%');
+					all_set++;
+					break;
 			}
-			j--;
 		}
-		_putchar(format[i]);
-		len++;
-		i++;
+		else
+		{
+			putchar(*format);
+			all_set++;
+		}
+
+		format++;
 	}
+
 	va_end(args);
-	return (len);
+	return all_set;
 }
