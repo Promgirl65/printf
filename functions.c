2f449cb0aea21d8c24fc4ea1b0d82396c5b98cd3
MsomiSK <133114393+MsomiSK@users.noreply.github.com>
2023-07-25 21:18:09 +0200

    Update functions.c

--- functions.c
+++ functions.c
@@ -16,6 +16,7 @@ int print_char(va_list types, char buffer[],
 {
 
 	char c = va_arg(types, int);
+	
 	return (handle_write_char(c, buffer, flags, width, precision, size));
 
 }
@@ -75,15 +76,14 @@ int print_string(va_list types, char buffer[],
 		else
 		{
 
-			for (i = width - length; i > 0; i--)
-				write(1, " ", 1);
-			write(1, &str[0], length);
-
-			return (width);
-
+for (i = width - length; i > 0; i--)
+	write(1, " ", 1);
+	write(1, &str[0], length);
+		
+	return (width);
 		}
 	}
-	return (write(1, str, length));
+return (write(1, str, length));
 }
 
 /**
@@ -132,18 +132,18 @@ int print_int(va_list types, char buffer[],
 	int is_negative = 0;
 	long int n = va_arg(types, long int);
 	unsigned long int num;
-	
+
 	n = convert_size_number(n, size);
-	
+
 	if (n == 0)
-		
+
 		buffer[i--] = '0';
 
 	buffer[BUFF_SIZE - 1] = '\0';
 	num = (unsigned long int)n;
-	
+
 	if (n < 0)
-	
+
 	{
 		num = (unsigned long int)((-1) * n);
 		is_negative = 1;
@@ -185,7 +185,7 @@ int print_binary(va_list types, char buffer[],
 	UNUSED(width);
 	UNUSED(precision);
 	UNUSED(size);
-	
+
 	n = va_arg(types, unsigned int);
 	m = 2147483648; /* (2 ^ 31) */
 	a[0] = n / m;
@@ -200,8 +200,11 @@ int print_binary(va_list types, char buffer[],
 	for (i = 0, sum = 0, count = 0; i < 32; i++)
 
 	{
+
 		sum += a[i];
+
 		if (sum || i == 31)
+
 		{
 			char z = '0' + a[i];
 			write(1, &z, 1);
